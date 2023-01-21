23a6f3da1a622ae62ba9a2db62874c9c6c07abb6
Renan Martins <renanmartins1999@hotmail.com>
2023-01-20 23:36:29 -0300

    Alterando o encapsulamento da classe Medicamento
    - Alterando o encapsulamento para ser possível realizar a edição do Medicamento, da qual é necessário resgatar a entidade do banco de dados.

--- Entities/Medicament.cs
+++ Entities/Medicament.cs
@@ -15,12 +15,12 @@
         }
 
         public Guid Id { get; private set; }
-        public string Name { get; private set; }
-        public string Dose { get; private set; }
-        public string Description { get; private set; }
+        public string Name { get; set; }
+        public string Dose { get; set; }
+        public string Description { get; set; }
         public DateTime CreatedAt { get; private set; }
         public int NumberOfPatients { get; private set; }
-        public Guid PatientId { get; private set; }
+        public Guid PatientId { get; set; }
         public Patient Patient { get; private set; }
     }
 }
